52ccd6563a3526d11bf493048ee6555e23d2a7b3
MaksimEiche33 <zederundbirke@gmail.com>
2023-12-23 23:55:34 +0300

    Erste Erhaltung

---
+++ app/src/main/java/com/example/prfung_25_12_23/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.prfung_25_12_23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
